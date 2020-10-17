5e10f7b6d24eeb38eeddf209573b1c224c1cc404
yen0413 <ct102ct102ct102@gmail.com>
2020-10-18 00:52:59 +0800

    fetch update get

--- assets/js/fetch_測試.js
+++ assets/js/fetch_測試.js
@@ -20,28 +20,65 @@ const sendHttpRequest = (method, url, data) => {  //fetch的上半部整理成fu
             return response.json();
         })
 }
+const content_tpl = tpl => {
+    return ` 
+    <div class="media main_BCC">
+        <figure class="figure_range">
+            <img src="assets/img/blog/匿名頭像1.jpg" class="align-self-start mr-3" alt="...">
+            <figcaption>${tpl.fName}</figcaption>
+        </figure>
+        <div class="media-body">
+
+            <h5 class="mt-0" id="title">標題 : ${tpl.fTitle}</h5>
+            <p>第${tpl.fID}篇  文章類型 : ${tpl.fType}</p>             
+            <p></p>
+            <p id="content">${tpl.fContent}</p>
+            <div class="input-group ">
+            <div class="input-group-prepend">
+                <div class="input-group-text">回文ID</div>
+            </div>
+            <input type="text" class="form-control text_mragin" id="${tpl.fID}" placeholder="留言......">
+            </div>
+            <p></p>
+            <div class="push_right">
+                <button type="submit" class="btn btn-secondary button_margin">留言</button>
+                <!-- <button type="submit" class="btn btn-secondary button_margin">加入圖片</button> -->
+            </div>
+            <P></P> 
+            
+            ${tpl.message.fmessage.map(o =>
+        `
+            
+            <div class="response_range">
+                <figure>
+                    <img src="assets/img/blog/頭像2.jpg" class="align-self-start mr-3" alt="...">
+                    <figcaption>${o.fMName}</figcaption>
+                </figure>
+                <p>${o.fMContent}</p>
+            </div>           
+            `
+    ).join(" ")} 
+        </div>
+    </div>
+    ` ;
+};
+//無法用forEach?
+
 
 const getData = () => {
-    fetch('https://reqres.in/api/users?page=2')  //get data
-        .then(response => {
-            console.log(response);               //transform
-            return response.json();
+    sendHttpRequest('get', 'https://localhost:44310/api/API')
+        .then(responseData => {
+            let str = '';
+            responseData.ftable.forEach(i => {
+                str += content_tpl(i);
+            })
+            $('#tpl').html(str);
+            //console.log(responseData);
         })
-        .then(responseData => {                  //using
-            console.log(responseData);
-        });
 };
 
-//利用sendHttpRequest將上面的程式改寫成下面
-// const getData = () => {
-//     sendHttpRequest('get', 'https://reqres.in/api/users?page=2', {})
-//         .then(responseData => {
-//             console.log(responseData);
-//         })
-// };
-
 const sendData = () => {
-    sendHttpRequest('post', 'https://reqres.in/api/user', {
+    sendHttpRequest('post', 'https://localhost:44310/api/API', {
         "title": document.getElementById("get-title").value,  //data的格式取決於要post出去的項目格式
         "content": document.getElementById("get-content").value
 
@@ -55,6 +92,7 @@ const sendData = () => {
 
 //getBtn.addEventListener('click', getData);
 postBtn.addEventListener('click', sendData);
+postBtn.addEventListener('click', getData);
 
 // function getValue() {
 //     var content = document.getElementById("get-content").value;
